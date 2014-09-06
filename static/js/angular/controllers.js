e3593cec992c4260ba2180039e57189d317fd298
Dawith Ha <dawithha@gmail.com>
2014-09-06 08:43:54 -0500

    linked to html

---
+++ static/js/angular/controllers.js
@@ -0,0 +1,13 @@
+var FourScore = angular.module('FourScore.controllers', []);
+FourScore.controller('main', function($scope, $http, $window, $location, $sce){
+	$scope.analyze = function(){
+		//$window.alert($('#mainText').val());
+		$scope.splitArray = $('#mainText').val().match(/((?:\S[^\.\?\!]*)[\.\?\!]*)/g);
+		//$scope.splitArray = $('#mainText').val().match(/[\.,]+/g);
+		$window.alert(JSON.stringify($scope.splitArray));
+		$('#mainText').val(function(i, val){
+			$scope.splitArray.match(/(?<=\.)(?=\w)/g);
+			return $scope.splitArray.join('');
+		});
+	}
+});
---
+++ static/js/angular/main.js
@@ -0,0 +1,9 @@
+angular.module('FourScore', [
+	'FourScore.config',
+	'FourScore.controllers',
+	'FourScore.directives',
+	'FourScore.filters',
+	'FourScore.services',
+	'FourScore.routes', 
+	'ngSanitize'
+]);
