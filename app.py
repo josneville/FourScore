1065517b26fb12972151491f17f03a83e22410f6
Dawith Ha <dawithha@gmail.com>
2014-09-06 15:50:08 -0500

    fix sentences POST method pls

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+from random import randint
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html') 
+	return json.dumps({
+		"sentences":[
+			"cat",
+			"dog",
+			"doge"
+		]
+	})
+   # return render_template('index.html') 
 
 @app.route('/analyze')
 def analyze():
     return render_template('index.html')
 
+@app.route('/api/runalgorithm', methods=['POST'])
+def run():
+	print 'Hello'
+	if 'sentences' in request.form:
+		# Get the JSON data from the form.
+		sentences = request.form['sentences']
+		data = json.loads(sentences)
+	else:
+		return "sentences not found",400
+	try:
+		returnJawn = {'emotions':[]} #fill returnJawn[emotions] with random numberse from 0 to 5.
+		for sentence in sentences: 
+			returnJawn[emotions].append[randint(0,5)]
+		return returnJawn
+	except:
+		return "Fail for unknown reasons"
 if __name__ == "__main__":
-    app.run()
+	app.debug=True
+	app.run()
