6ed0f4935b38569e1408dc61e98cb388fcb2b595
Greedoid <jasieni2@illinois.edu>
2014-09-06 17:25:26 -0500

    in theory

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
 import json
 from random import randint
+from machine import Machine
 app = Flask(__name__)
+
+classifier = Machine() 
+classifier.make_svm()
+
 
 @app.route('/')
 def main():
@@ -18,12 +23,15 @@ def run():
 		# Get the JSON data from the form.
 		sentences = request.form['sentences']
 		data = json.loads(sentences)
+		print data
+		probs = classifier.get_probs(data)
+		print probs
 	else:
 		return "sentences not found",400
 	try:
-		returnJawn = json.loads('{"emotions":[]}') #fill returnJawn[emotions] with random numberse from 0 to 5.
-		for sentence in data: 
-			returnJawn['emotions'].append(randint(0,5))
+		returnJawn = json.loads('{"emotions":'+probs+',}') #fill returnJawn[emotions] with random numberse from 0 to 5.
+		#for sentence in data: 
+		#	returnJawn['emotions'].append(randint(0,5))
 		return json.dumps(returnJawn)
 	except:
 		return "Fail for unknown reasons"
