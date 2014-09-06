e2889a6cba32e120d7af564a85203e070fc0182e
Vishal Disawar <Vishal@0587363405.wireless.umich.net>
2014-09-06 13:24:52 -0400

    did sentence stuff

--- static/js/angular/controllers.js
+++ static/js/angular/controllers.js
@@ -1,19 +1,39 @@
 var FourScore = angular.module('FourScore.controllers', []);
-FourScore.controller('main', function($scope, $http, $window, $location, $sce){
+FourScore.controller('main', function($scope, $http, $window, $location){
 	if (typeof $scope.splitArray === "undefined" || $scope.splitArray.length == 0){
 		$scope.splitArray = [];
 	}
-	$scope.formatText = function(){
-		$('#mainText').val(function(i, val){
-			return $scope.splitArray.join('');
-		});
-	}
-	$scope.formatText();
+	
 	$scope.analyze = function(){
 		$scope.splitArray = $('#mainText').val().match(/([^\.\?\!\n]+|\.+|\?+|\!+|\n+)/g);
-		$scope.formatText();
+		$scope.sentenceArray = [];
+		for (var i = 0; i < $scope.splitArray.length; i++) {
+			var phrase = $scope.splitArray[i];
+			if(phrase !== '.' && phrase !== '?' && phrase !== '!' && phrase.substring(0,1) !== '\n'){
+				$scope.sentenceArray.push({
+					'index' : i,
+					'value' : phrase
+				});
+			}
+		}
+
+		$location.path('/analyze');
 	}
 	$scope.disableAnalyze = function(){
 		return typeof $scope.mainText === "undefined" || $scope.mainText.length == 0;
 	}
+	$scope.test = 'blah';
+});
+
+FourScore.controller('analyze', function($scope, $http, $window, $location) {
+	// body...
+	//$window.alert(JSON.stringify($scope.sentenceArray));
+
+});
+
+FourScore.controller('input', function($scope, $http, $window, $location){
+
+	$('#mainText').val(function(i, val){
+		return $scope.splitArray.join('');
+	});
 });
--- static/js/angular/routes.js
+++ static/js/angular/routes.js
@@ -2,8 +2,10 @@ var FourScore = angular.module('FourScore.routes', ['ngRoute']);
 
 FourScore.config(function($routeProvider) {
 	$routeProvider.when('/', {
-		templateUrl : '/static/views/input.html'
+		templateUrl : '/static/views/input.html',
+		controller : 'input'
 	}).when('/analyze', {
-		templateUrl : '/static/views/analyze.html'
+		templateUrl : '/static/views/analyze.html',
+		controller : 'analyze'
 	});
 });
